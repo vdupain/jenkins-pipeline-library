869609f689f7d577c007eb41dbf1de9a2a503222
ⓥⓘⓝⓒⓔ <vdupain@gmail.com>
2018-03-26 20:10:30 +0200

    Update getSha1AsVersion.groovy

--- vars/getSha1AsVersion.groovy
+++ vars/getSha1AsVersion.groovy
@@ -1,12 +1,6 @@
 #!/usr/bin/groovy
 
 def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
     def version 
     try {
         version = sh(script: 'git rev-parse --short HEAD', returnStdout: true).toString().trim()
